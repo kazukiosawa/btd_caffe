429d825184af28258ca81722ac251298ada5833c
KazukiOsawa <osawa1021@gmail.com>
2017-09-19 00:06:52 +0900

    remove template

--- approximate_net.py
+++ approximate_net.py
@@ -4,12 +4,12 @@ import sys
 import numpy as np
 import csv
 import caffe
-from caffe.proto import caffe_pb2
+from caffe import layers as L
+from caffe.proto.caffe_pb2 import NetParameter, LayerParameter
 import google.protobuf.text_format as txtf
 from pci import pci
 from sktensor import dtensor
 from argparse import ArgumentParser
-
 
 
 # load parameters for btd
@@ -18,73 +18,95 @@ def load_config(config_file):
 	with open(config_file, 'r') as f:
 		reader = csv.reader(f)
 		for row in reader:
-			param, c, n, blocks = row[0], int(row[1]), int(row[2]), int(row[3])
-			btd_config[param] = (c, n, blocks)
+			param, s, t, blocks = row[0], int(row[1]), int(row[2]), int(row[3])
+			btd_config[param] = (s, t, blocks)
 	return btd_config
 
 
-def create_new_net(template_deploy, template_train_test, save_deploy, save_train_test, btd_config):
-	new_deploy, new_train_test = \
-		get_template_net(args.template_deploy, args.template_train_test)
-	modify_convolution_num_output(new_deploy, btd_config)
-	modify_convolution_num_output(new_train_test, btd_config)
-	# save new prototxt
-	with open(save_deploy, 'w') as f:
-		f.write(str(new_deploy))
-	with open(save_train_test, 'w') as f:
-		f.write(str(new_train_test))
-	# load new prototxt
-	save_net = caffe.Net(save_deploy, caffe.TEST)
-	return save_net
+def create_approx_netdef(input_file, output_file, btd_config):
+	with open(input_file, 'r') as fp:
+		net = NetParameter()
+		txtf.Merge(fp.read(), net)
+	new_layers = []
+	for layer in net.layer:	
+		if not layer.name in btd_config.keys():
+			new_layers.append(layer)
+			continue
+		s, t, r = btd_config[layer.name]
+		a, b, c = decompose2abc(layer, s, t, r)
+		new_layers.extend([a, b, c])
+	new_net = NetParameter()
+	new_net.CopyFrom(net)
+	del(new_net.layer[:])
+	new_net.layer.extend(new_layers)
+	with open(output_file, 'w') as fp:
+		fp.write(txtf.MessageToString(new_net))
+		
+
+def decompose2abc(conv, s, t, r):
+	print conv.name
+	def _create_new(name):
+		print "--> {0}".format(name)
+		new_ = LayerParameter()
+		new_.CopyFrom(conv)
+		new_.name = name
+		return new_
+	conv_param = conv.convolution_param
+	# 1st
+	a = _create_new(conv.name + 'a')
+	del(a.top[:])
+	a.top.extend([a.name])
+	if len(a.param) > 2:
+		a.param[1].lr_mult = 0
+	a_param = a.convolution_param
+	a_param.num_output = s
+	del(a_param.kernel_size[:])
+	a_param.kernel_size.extend([1])
+	del(a_param.pad[:])
+	a_param.pad.extend([0])
+	del(a_param.stride[:])
+	a_param.stride.extend([1])
+	# 2nd
+	b = _create_new(conv.name + 'b')
+	del(b.bottom[:])
+	b.bottom.extend(a.top)
+	del(b.top[:])
+	b.top.extend([b.name])
+	if len(b.param) > 2:
+		b.param[1].lr_mult = 0
+	b_param = b.convolution_param
+	b_param.num_output = t
+	b_param.group = r
+	# 3rd
+	c = _create_new(conv.name + 'c')
+	del(c.bottom[:])
+	c.bottom.extend(b.top)
+	c_param = c.convolution_param
+	del(c_param.kernel_size[:])
+	c_param.kernel_size.extend([1])
+	del(c_param.pad[:])
+	c_param.pad.extend([0])
+	del(c_param.stride[:])
+	c_param.stride.extend([1])
+	return a, b, c
 
 
-def get_template_net(template_deploy, template_train_test):
-	# create new prototxt
-	new_deploy = caffe_pb2.NetParameter()
-	new_train_test = caffe_pb2.NetParameter()
-
-	# load template prototxt
-	with open(template_deploy) as f:
-		s = f.read()
-		txtf.Merge(s, new_deploy)
-	with open(template_train_test) as f:
-		s = f.read()
-		txtf.Merge(s, new_train_test)
-	return new_deploy, new_train_test
-
-
-def modify_convolution_num_output(new_proto, btd_config):
-	layer_names = [l.name for l in new_proto.layer]
-	for conv, values in btd_config.items():
-		c, n, blocks = values
-		idx = layer_names.index(conv + 'a')
-		l = new_proto.layer[idx]
-		l.convolution_param.num_output = c
-		idx = layer_names.index(conv + 'b')
-		l = new_proto.layer[idx]
-		l.convolution_param.num_output = n
-		l.convolution_param.group = blocks
-
-
-# approximate original weights (kernels) & copy original bias
-def create_lowrank_net(net_lowrank, net, btd_config, max_iter, min_decrease):
-	copy = [param for param in net.params.keys() if not param in btd_config.keys()]
-	for param in copy:
-		net_lowrank.params[param][0].data[...] = net.params[param][0].data
-		net_lowrank.params[param][1].data[...] = net.params[param][1].data
-
+def approximate_params(netdef, params, approx_netdef, approx_params,
+			 btd_config, max_iter, min_decrease):
+	net = caffe.Net(netdef, params, caffe.TEST)
+	net_approx = caffe.Net(approx_netdef, params, caffe.TEST)
 	convs = [(k, v[0].data, v[1].data) for k, v in net.params.items() if k in btd_config.keys()]
 	for conv, kernel, bias in convs:
 		size = kernel.shape
-		N, C, H, W = size[0:4]
+		T, S, H, W = size[0:4]
 		P = H * W
-		c, n, blocks = btd_config[conv]
-		# (N, C, H, W) -> (N, C, P)
-	        kernel = kernel.reshape(N, C, P)	
+		s, t, blocks = btd_config[conv]
+		# (T, S, H, W) -> (T, S, P)
+	        kernel = kernel.reshape(T, S, P)	
 		# compute BTD 
-		n_ = int(n/blocks)
-		c_ = int(c/blocks)
-		rank = [n_, c_, P]
+		t_ = int(t/blocks)
+		s_ = int(s/blocks)
+		rank = [t_, s_, P]
 		print ('calculating BTD for {0}...').format(conv)
 		btd, _ = pci(dtensor(kernel), blocks, rank, max_iter, min_decrease) 
 		# BTD -> (c, C) (n, c/blocks, P) (N, n)
@@ -92,21 +114,25 @@ def create_lowrank_net(net_lowrank, net, btd_config, max_iter, min_decrease):
 		kernel_b = np.concatenate([subtensor[0].ttm(subtensor[1][2], 2) for subtensor in btd], axis=0)
 		kernel_c = np.concatenate([subtensor[1][0] for subtensor in btd], axis=1)
 		# (c, C) -> (c, C, 1, 1)
-		kernel_a = kernel_a.T.reshape(c, C, 1, 1)
+		kernel_a = kernel_a.T.reshape(s, S, 1, 1)
 		# (n, c/blocks, P) -> (n, c/blocks, H, W)
-		kernel_b = kernel_b.reshape(n, c_, H, W)
+		kernel_b = kernel_b.reshape(t, s_, H, W)
 		# (N, n) -> (N, n, 1, 1)
-		kernel_c = kernel_c.reshape(N, n, 1, 1)
+		kernel_c = kernel_c.reshape(T, t, 1, 1)
 		# set kernel to low-rank model
-		net_lowrank.params[conv + 'a'][0].data[...] = kernel_a
-		net_lowrank.params[conv + 'b'][0].data[...] = kernel_b
-		net_lowrank.params[conv + 'c'][0].data[...] = kernel_c
+		net_approx.params[conv + 'a'][0].data[...] = kernel_a
+		net_approx.params[conv + 'b'][0].data[...] = kernel_b
+		net_approx.params[conv + 'c'][0].data[...] = kernel_c
 		# copy bias to low-rank model
-		net_lowrank.params[conv + 'c'][1].data[...] = bias
-	return net_lowrank	
+		net_approx.params[conv + 'c'][1].data[...] = bias
+	net_approx.save(approx_params)
 
 
 def main(args):
+	btd_config = load_config(args.config)
+	create_approx_netdef(args.netdef, args.save_netdef, btd_config)
+
+	if args.params is None: return
 	if args.max_iter is None:
 		max_iter=100
 	else:
@@ -116,39 +142,25 @@ def main(args):
 	else:
 		min_decrease=float(args.min_decrease)
 
-	btd_config = load_config(args.config)
-	new_net = create_new_net(args.template_deploy, args.template_train_test \
-				, args.save_deploy, args.save_train_test, btd_config)
-	# load original model
-	net = caffe.Net(args.model, args.weights, caffe.TEST)
-	net_lowrank = create_lowrank_net(new_net, net, btd_config, max_iter, min_decrease)
-	# save low-rank model
-	print ('saving {0} ...').format(args.save_weights)
-	net_lowrank.save(args.save_weights)
-	print ('done.')
+	approximate_params(args.netdef, args.params, args.save_netdef, args.save_params, 
+				btd_config, max_iter, min_decrease)
 
 
 if __name__ == '__main__':
     parser = ArgumentParser(description="Block Term Decomposition on Convolution Kernel")
-    parser.add_argument('--model', required=True,
+    parser.add_argument('--netdef', required=True,
         help="Prototxt of the original net")
-    parser.add_argument('--weights', required=True,
-        help="Caffemodel of the original net")
-    parser.add_argument('--save_deploy', required=True,
+    parser.add_argument('--save_netdef', required=True,
         help="Path to the deploy.prototxt of the low-rank approximated net")
-    parser.add_argument('--save_train_test', required=True,
-        help="Path to the deploy.prototxt of the low-rank approximated net")
-    parser.add_argument('--save_weights', required=True,
-        help="Path to the caffemodel of the low-rank approximated net")
     parser.add_argument('--config', required=True,
         help="CSV config file for BTD")
+    parser.add_argument('--params', 
+        help="Caffemodel of the original net")
+    parser.add_argument('--save_params',
+        help="Path to the caffemodel of the low-rank approximated net")
     parser.add_argument('--max_iter',
         help="Max iteration for BTD")
     parser.add_argument('--min_decrease',
         help="Minimum error decrease in each iteration for BTD")
-    parser.add_argument('--template_deploy', required=True,
-        help="Path to the deploy.prototxt of the template net")
-    parser.add_argument('--template_train_test', required=True,
-        help="Path to the train_test.prototxt of the template net")
     args = parser.parse_args()
     main(args)
--- start.sh
+++ start.sh
@@ -3,34 +3,39 @@
 # root setting
 CAFFE_MODEL_ROOT=/opt/storage/models/caffe
 MODEL_ROOT=$CAFFE_MODEL_ROOT/vgg16
+PROTO_DIR=/workspace/btd_caffe/vgg16
 
 # original model
-ORIGINAL_DEPLOY=$MODEL_ROOT/deploy.prototxt
-ORIGINAL_MODEL=$MODEL_ROOT/VGG_ILSVRC_16_layers.caffemodel
+ORIGINAL_DEPLOY=$PROTO_DIR/deploy.prototxt
+ORIGINAL_TRAIN_TEST=$PROTO_DIR/train_test.prototxt
+ORIGINAL_PARAMS=$MODEL_ROOT/VGG_ILSVRC_16_layers.caffemodel
 
-# template for low-rank model
-TEMPLATE_DEPLOY=$MODEL_ROOT/lowrank/template_deploy.prototxt
-TEMPLATE_TRAIN_TEST=$MODEL_ROOT/lowrank/template_train_test.prototxt
-
-# low-rank model 
-LOWRANK_DEPLOY=$MODEL_ROOT/lowrank/deploy2.prototxt
-LOWRANK_TRAIN_TEST=$MODEL_ROOT/lowrank/train_test2.prototxt
-LOWRANK_MODEL=$MODEL_ROOT/lowrank/VGG_ILSVRC_16_layers_lowrank2.caffemodel
-CONFIG=$MODEL_ROOT/lowrank/params2.csv
+# low-rank model
+KEY=$1 
+CONFIG=$PROTO_DIR/params_$KEY.csv
+APPROX_DEPLOY=$PROTO_DIR/deploy_$KEY.prototxt
+APPROX_TRAIN_TEST=$PROTO_DIR/train_test_$KEY.prototxt
+APPROX_PARAMS=$MODEL_ROOT/VGG_ILSVRC_16_layers_$KEY.caffemodel
 
 # PCI setting
-MAX_ITER=1000
-MIN_DECREASE=1e-2
+MAX_ITER=100
+MIN_DECREASE=1e-3
 
 COMMAND=./approximate_net.py
 
-$COMMAND --model $ORIGINAL_DEPLOY \
-	 --weights $ORIGINAL_MODEL \
-         --save_deploy $LOWRANK_DEPLOY \
-	 --save_train_test $LOWRANK_TRAIN_TEST \
-	 --save_weights $LOWRANK_MODEL \
-	 --config $CONFIG \
-         --max_iter $MAX_ITER \
-	 --min_decrease $MIN_DECREASE \
-         --template_deploy $TEMPLATE_DEPLOY \
-	 --template_train_test $TEMPLATE_TRAIN_TEST
+DEPLOY="deploy"
+
+if [ "$2" = $DEPLOY ]
+ then
+  $COMMAND --netdef $ORIGINAL_DEPLOY \
+           --save_netdef $APPROX_DEPLOY \
+	   --config $CONFIG 
+else
+  $COMMAND --netdef $ORIGINAL_TRAIN_TEST \
+           --save_netdef $APPROX_TRAIN_TEST \
+	   --config $CONFIG \
+	   --params $ORIGINAL_PARAMS \
+	   --save_params $APPROX_PARAMS \
+           --max_iter $MAX_ITER \
+	   --min_decrease $MIN_DECREASE 
+fi
